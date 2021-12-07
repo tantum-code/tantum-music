03ccaafe0e8ce560210dff20b9a41dc07ea1fae6
Thomas <33089253+tantum-code@users.noreply.github.com>
2021-12-07 21:01:18 +0100

    added carousel

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 
+//new
+const track = document.querySelector('.track');
+let initialPosition = null;
+let moving = false;
+let transform = 0;
+//
+
 timeline
     .to(".layer1",3,{y:-300})
     .to(".layer2",3,{y:-200},"-=3")
@@ -15,3 +22,47 @@ let scene = new ScrollMagic.Scene({
 })
     .setTween(timeline)
     .addTo(controller);
+
+
+//new
+const gestureStart = (e) => {
+    initialPosition = e.pageX;
+    moving = true;
+    const transformMatrix = window.getComputedStyle(track).getPropertyValue('transform');
+    if (transformMatrix !== 'none') {
+      transform = parseInt(transformMatrix.split(',')[4].trim());
+    }
+}
+      
+const gestureMove = (e) => {
+    if (moving) {
+      const currentPosition = e.pageX;
+      const diff = currentPosition - initialPosition;
+      track.style.transform = `translateX(${transform + diff}px)`;  
+    }
+};
+      
+const gestureEnd = (e) => {
+    moving = false;
+}
+      
+if (window.PointerEvent) {
+    window.addEventListener('pointerdown', gestureStart);
+      
+    window.addEventListener('pointermove', gestureMove);
+      
+    window.addEventListener('pointerup', gestureEnd);  
+} else {
+    window.addEventListener('touchdown', gestureStart);
+      
+    window.addEventListener('touchmove', gestureMove);
+      
+    window.addEventListener('touchup', gestureEnd);  
+        
+    window.addEventListener('mousedown', gestureStart);
+      
+    window.addEventListener('mousemove', gestureMove);
+      
+    window.addEventListener('mouseup', gestureEnd);  
+}
+      
